b2f1a0999eee5b456b785b4df5d3a96a52aa24ba
sainadh-paruchuri <saiparuchuri121@gmail.com>
2023-01-31 19:42:38 +0530

    fetch products added

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://sainadh:sainadh3811@cluster0.snpasul.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://sainadh:sainadh3811@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
